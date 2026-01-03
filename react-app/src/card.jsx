e39c0847e6800d5635c2090ee50714d0e43db271
Hari prasad jangili <hariprasadyadavjangili@gmail.com>
2026-01-03 17:39:42 +0000

    feat: update card component and styles, add new images
    - Updated the Card component to use a new image from assets.
    - Added two new image files to the assets directory.
    - Modified CSS for the Card component to improve layout and styling.
    - Adjusted image styling for better presentation.
    - Fixed formatting in vite.config.js.

--- react-app/src/card.jsx
+++ react-app/src/card.jsx
@@ -1,7 +1,7 @@
 function Card(){
     return(
         <div className="Card">
-            <img className="img1" src="./public/vite.svg" alt="Biryani" />
+            <img className="img1" src="./src/assets/uwp4751282.jpeg" alt="Biryani" />
             <h2>Biryani</h2>
             <p>A special hyderabad biryani</p>
         </div>

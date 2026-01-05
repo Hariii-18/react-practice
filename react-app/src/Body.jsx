4279edb75b5fa4c0a5027bf142278dd355950704
Hari prasad jangili <hariprasadyadavjangili@gmail.com>
2026-01-05 13:05:13 +0000

    feat: update Body component to include a specific Card instance, modify Card component to accept props, and fix CSS class name for card styling

--- react-app/src/Body.jsx
+++ react-app/src/Body.jsx
@@ -1,8 +1,10 @@
 import Card from "./card";
 import Button from "./Button/button";
+
 function Body(){
     return(
         <>
+        <Card  Heading="Biryani" description="delicious hyderabadi biryani" Rating={4.5} price={$40} />
         <Card/>
         <Button/>
         </>     
--- react-app/src/card.jsx
+++ react-app/src/card.jsx
@@ -1,14 +1,32 @@
-function Card(){
-    const Rating = 4.50;
+import PropTypes from 'prop-types';
+
+function Card(props){
+    // const Rating = 4.50;
     return(
-        <div className="Card">
-            <img className="img1" src="./src/assets/biryani.webp" alt="Biryani" />
-            <h2 className="card-Heading">Chicken Biryani</h2>
-            <p className="card-p">A special hyderabad biryani</p>
-            <p className="rating">Rating:{Rating} </p>
-            <p className="price">$40</p>
-        </div>
+            <div className="card">
+            <p className="img1">{props.img}</p>
+            <p className="card-Heading">{props.Heading}</p>
+            <p>{props.description}</p>
+            <p >{props.Rating}</p>
+            <p>{props.price}</p>
+            </div>
+
+
+
+
+
+        // <div className="Card">
+        //     <img className="img1" src="./src/assets/biryani.webp" alt="Biryani" />
+        //     <h2 className="card-Heading">Chicken Biryani</h2>
+        //     <p className="card-p">A special hyderabad biryani</p>
+        //     <p className="rating">Rating:{Rating} </p>
+        //     <p className="price">$40</p>
+        // </div>
     );
+}
+Card.PropTypes = {
+    price: PropTypes.number,
+    
 }
 
 export default Card ;

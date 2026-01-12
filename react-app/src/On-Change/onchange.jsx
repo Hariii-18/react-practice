d52b767222ca01ad93f42ff85b04c345884f039c
Hari prasad jangili <hariprasadyadavjangili@gmail.com>
2026-01-12 15:00:39 +0000

    worked on "onchange" event handler created different number of problems , styling has been done

--- react-app/src/On-Change/onchange.jsx
+++ react-app/src/On-Change/onchange.jsx
@@ -3,27 +3,59 @@
 //            Triggers a function every time when the value of the input changes
 
 import { useState } from "react";
+import "./onchange.css"
 
-function onChange(){
+function onChange() {
 
     const [name, setName] = useState("");
-    const [age,setAge] = useState("");
-    function textonchange(event){
+
+    const [age, setAge] = useState("");
+
+    const [comment,setcomment] = useState("");
+
+    const [state, setstate] = useState("");
+
+    function textonchange(event) {
         setName(event.target.value);
     }
-    function AgeSetter(event){
+
+    function AgeSetter(event) {
         setAge(event.target.value);
     }
-    return( 
-        <div>
-        <input placeholder="Enter your Name here" value={name} type="text"  onChange={textonchange} />
-        <p>Name: {name}</p>
 
-        <input placeholder="Enter your age" type="number" value={age} onChange={AgeSetter}/>
-        <p>Age:{age}</p>
+    function CommentBox(event){
+        setcomment(event.target.value)
+    }
+
+    function selectState(e){
+        setstate(e.target.value);
+    }
+
+    return (
+        <div className="container">
+            <p className="head">Onchange Event Handler:</p>
+            <input className="inputs" placeholder="Enter your Name here" value={name} type="text" onChange={textonchange} />
+            <p className="para">Name: {name}</p>
+
+            <input className="inputs" placeholder="Enter your age" type="number" value={age} onChange={AgeSetter} />
+            <p className="para">Age:{age}</p>
+
+            <input className="inputs" placeholder="Write your Comment Here " value={comment} onChange={CommentBox}/>
+            <p className="para">Comment: {comment}</p>
+
+            <select className="selector" value={state} onChange={selectState}>
+                <option value="" disabled selected>Select your State:</option>
+                <option value="Telangana">Telangana</option>
+                <option value="Andhra Pradesh">Andhra Pradesh</option>
+                <option value="Karnataka">Karnataka</option>
+                <option value="amil Nadu">Tamil Nadu</option>
+                <option value="Kerala">Kerala</option>
+                <option value="Uttar Pradesh">Uttar Pradesh</option>
+            </select>
+            <p className="para">State: {state}</p>
         </div>
     );
 }
-
+// 2:09:26
 
 export default onChange;

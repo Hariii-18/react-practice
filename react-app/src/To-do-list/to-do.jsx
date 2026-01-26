c0277142170122f2df58f9a632b6fe380357591e
Hari prasad jangili <hariprasadyadavjangili@gmail.com>
2026-01-26 16:53:27 +0000

    wroked on basic logic behind the to-do list , with working add and delete button

--- react-app/src/To-do-list/to-do.jsx
+++ react-app/src/To-do-list/to-do.jsx
@@ -1,45 +1,71 @@
 import React, { useState } from "react";
-import "./to-do.css";
+import "./to-do.css"
 
+function Todo() {
+    const [tasks, setTasks] = useState([]);   //tasks = stores all the to-do items , setTasks = updates the list
+    const [input, setInput] = useState("");
 
-function App() {
-
-    const [Tasks, SetTask] = useState(["write home work"]);
-    const [newTask, setNewTask] = useState("");
-
-    function inputhandler(event) {
-        setNewTask(event.target.value);
+    const addTask = () => {
+        if (input === "") return;
+        if (input.trim() === "") return;
+        const NewTask = {
+            id: Date.now(),
+            text: input,
+            completed: false,
+            important: false,
+        };
+        setTasks([...tasks, NewTask]);
+        setInput("");
     }
-    function addTask() {
-        SetTask([...Tasks,newTask]);
-        setNewTask("");
+
+    const deleteTask = (id) => {
+        setTasks(prev => prev.filter(task => task.id !== id));
     }
 
-    function deleteTask(index) {
-        
-    }
+    const CompleteTask = (id) => {
+        const updatedTasks = tasks.map(task =>
+            task.id === id ?
+                { ...task, completed: !task.completed }
+                : task
+        );
+        setTasks(updatedTasks);
+    };
+
+    const ImportantTaks = (id) => {
+        const updatedTasks = tasks.map(task =>
+            task.id === id
+                ? { ...task, important: !task.important }
+                : task
+        );
+        setTasks(updatedTasks);
+    };
 
     return (
-        <div className="Container">
-            <div>
-                <h1 className="heading">To Do List</h1>
-                <input type="text"
-                    placeholder="Enter your Task"
-                    value={newTask}
-                    onChange={inputhandler} />
-                <button className="add-button" onClick={() => addTask(index)}>Add</button>
-            </div>
-            <ol>
-                {Tasks.map((tasks, index) =>
-                    <li key={index}>
-                        <span>{Tasks}</span>
-                        <button className="delete-button" onClick={() => deleteTask(index)}>delete</button>
-                    </li>
-                )}
+        <div>
+            <h1>TO-DO List</h1>
+            <input value={input}
+                onChange={(e) => setInput(e.target.value)}
+                type="text" placeholder="Enter your Task " />
 
+            <button onClick={addTask}>Add</button>
+
+            <ol>
+                {tasks.map(task => (
+                    <li key={task.id}>
+                        <span
+                            style={{
+                                textDecoration: task.completed ? "line-through" : "none"
+                            }}>
+                            {task.text}
+                        </span>
+                        <button onClick={() => CompleteTask(task.id)}>complete</button>
+                        <button onClick={() => ImportantTaks(task.id)}>star</button>
+                        <button onClick={() => deleteTask(task.id)}>delete</button>
+                    </li>
+                ))}
             </ol>
         </div>
     );
 }
 
-export default App
+export default Todo;
